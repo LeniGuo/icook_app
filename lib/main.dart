5cf2a491b2401b1b057cec4ea2c0f137c1e6f0b4
KKlovesleep <112485991+KKlovesleep@users.noreply.github.com>
2024-06-03 00:27:30 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 import 'sidebar.dart';
 
 void main() {
@@ -31,6 +33,8 @@ class _ChatScreenState extends State<ChatScreen> {
   final List<String> _currentConversation = []; // 当前会话消息
   final TextEditingController _textController = TextEditingController();
   final Set<String> _selectedTags = Set<String>(); // 存储选中的标签
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
 
   void _handleSubmitted(String text) {
     _textController.clear();
@@ -42,10 +46,10 @@ class _ChatScreenState extends State<ChatScreen> {
     Future.delayed(Duration(milliseconds: 500), () {
       setState(() {
         _messages.add(ChatMessage(
-          text: "请选择一个或多个菜品种类:",
+          text: "please select tags",
           isUser: false,
           isSystem: true,
-          tags: ['中餐', '西餐', '甜点'],
+          tags: ['tomato', 'egg'],
           onTagTap: _handleTagSelection,
           onConfirm: _confirmSelection,
           selectedTags: _selectedTags,
@@ -70,7 +74,7 @@ class _ChatScreenState extends State<ChatScreen> {
         String selectedTagsString = _selectedTags.join(", ");
         _currentConversation.add(selectedTagsString);
         _messages.add(ChatMessage(
-          text: "这是你选择的 $selectedTagsString 的食谱预览。",
+          text: 'Preview of Recipe: Tomato and Egg Stir Fry\nIngredients: 2 large eggs\n1 medium tomato\n2 tablespoons vegetable oil\n1/2 teaspoon salt\n1/4 teaspoon sugar\n1/4 teaspoon soy sauce (optional)\n1/4 teaspoon white pepper (optional)\n',
           isUser: false,
         ));
         _selectedTags.clear();
@@ -83,6 +87,22 @@ class _ChatScreenState extends State<ChatScreen> {
       _history.add(List.from(_currentConversation)); // 将当前会话保存到历史
       _currentConversation.clear();
       _messages.clear();
+    });
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = XFile(pickedFile.path); // 存储选定的图像
+        _messages.add(ChatMessage(
+          image: _image, // 添加图片对象
+          isUser: true,
+        ));
+      } else {
+        print('No image selected.');
+      }
     });
   }
 
@@ -138,9 +158,7 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
             IconButton(
               icon: Icon(Icons.camera_alt),
-              onPressed: () {
-                // 添加相机功能处理代码
-              },
+              onPressed: _pickImage,
             ),
             IconButton(
               icon: Icon(Icons.send),
@@ -154,66 +172,65 @@ class _ChatScreenState extends State<ChatScreen> {
 }
 
 class ChatMessage extends StatelessWidget {
-  final String text;
+  final String? text;
   final bool isUser;
   final bool isSystem;
   final List<String>? tags;
   final Function(String)? onTagTap;
   final VoidCallback? onConfirm;
-  final Set<String>? selectedTags;
+  final XFile? image; // 使用XFile来存储图像文件
+  final Set<String>? selectedTags; // 添加选中的标签参数
 
-  ChatMessage({required this.text, required this.isUser, this.isSystem = false, this.tags, this.onTagTap, this.onConfirm, this.selectedTags});
+  ChatMessage({this.text, required this.isUser, this.isSystem = false, this.tags, this.onTagTap, this.onConfirm, this.image, this.selectedTags});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: isUser ? EdgeInsets.only(top: 10.0, bottom: 10.0, left: 80.0) : EdgeInsets.only(top: 10.0, bottom: 10.0, right: 80.0),
-      child: isSystem && tags != null
-          ? Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Wrap(
-            spacing: 6.0,
-            children: tags!.map((tag) {
-              final bool isSelected = selectedTags != null && selectedTags!.contains(tag);
-              return GestureDetector(
-                onTap: () {
-                  // 处理标签点击
-                  if (onTagTap != null) {
-                    onTagTap!(tag);
-                  }
-                },
-                child: Chip(
-                  label: Text(tag),
-                  backgroundColor: isSelected ? Colors.blue : null,
-                  labelStyle: TextStyle(color: isSelected ? Colors.white : null),
-                ),
-              );
-            }).toList(),
-          ),
-          SizedBox(height: 12.0), // 增加间隔
-          ElevatedButton(
-            onPressed: onConfirm,
-            child: Text('确认'),
-          ),
-        ],
-      )
-          : Row(
-        mainAxisAlignment: isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: isUser ? Colors.blue : Colors.grey[300],
-              borderRadius: BorderRadius.circular(12.0),
+        margin: isUser ? EdgeInsets.only(top: 10.0, bottom: 10.0, left: 80.0) : EdgeInsets.only(top: 10.0, bottom: 10.0, right: 80.0),
+        child: isSystem && tags != null
+            ? Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Wrap(
+              spacing: 6.0,
+              children: tags!.map((tag) {
+                return GestureDetector(
+                  onTap: () {
+                    if (onTagTap != null) {
+                      onTagTap!(tag);
+                    }
+                  },
+                  child: Chip(
+                    label: Text(tag),
+                  ),
+                );
+              }).toList(),
             ),
-            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
-            child: Text(
-              text,
-              style: TextStyle(color: isUser ? Colors.white : Colors.black),
+            SizedBox(height: 12.0), // 增加间隔
+            ElevatedButton(
+              onPressed: onConfirm,
+              child: Text('确认'),
             ),
-          ),
-        ],
-      ),
+          ],
+        )
+            : Row(
+            mainAxisAlignment: isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: <Widget>[
+        image != null ? Image.file(File(image!.path), width: 200.0, height: 200.0) : Container(), // 如果image不为空，则显示图片
+    text != null ? Container(
+    decoration: BoxDecoration(
+    color: isUser ? Colors.blue : Colors.grey[300],
+    borderRadius: BorderRadius.circular(12.0),
+    ),
+    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+    margin: EdgeInsets.only(left: isUser ? 8.0 : 0.0, right: isUser ? 0.0 : 8.0),
+    child: Text(
+    text!,
+    style: TextStyle(color: isUser ? Colors.white : Colors.black),
+    ),
+    ) : Container(), // 如果text不为空，则显示文本
+            ],
+        ),
     );
   }
 }
